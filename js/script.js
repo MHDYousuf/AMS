10795e96427b202a7256717b57c300ce28df1f76
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2018-10-16 22:14:48 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$("#sbm").click(function(){
 		if($("#inputGroupSelect04").val()=="Choose Your Collge"){
 			$(".err-p").show();
+			$(".hidee").hide();
 		}
 		else{
 			$(".hidee").show();
