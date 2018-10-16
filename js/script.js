d2e898881a7797a27cc63cf24d702a8e021f3fdb
muzam-il <36731405+muzam-il@users.noreply.github.com>
2018-10-16 22:07:57 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 	$("#sbm").click(function(){
 		if($("#inputGroupSelect04").val()=="Choose Your Collge"){
 			$(".err-p").show();
-			$(".hidee").hide();
 		}
 		else{
 			$(".hidee").show();
+			$("#inputGroupSelect04").hide();
+			$("#sbm").hide();
 			$(".err-p").hide();
 		}
 	});
