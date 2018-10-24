2c7d5af9c50565d6bcdd442970c0470728bd83f6
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2018-10-24 17:50:44 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+$(document).ready(function(){
 
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 80) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},600);
+        return false;
+    });
+
+});
 $(document).ready(function(){
 	$("#sbm").click(function(){
 		if($("#inputGroupSelect04").val()=="Choose Your Collge"){
